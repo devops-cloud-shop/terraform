dc4e840139bc7582c24220f16cf6811aefacacee
Prav-Akula <pravs.akula@outlook.com>
2025-10-23 16:31:42 -0400

    terraform

---
+++ provisioners/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.16.0" #"~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "remote-state-86s-bucket"
+    key    = "provisioners-demo"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+    region = "us-east-1"
+  }
